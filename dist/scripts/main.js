edae870c394538e9f048bdbb54f46e5f817b8e4c
Fernando Neves <fernandodn3006@gmail.com>
2025-05-28 10:47:48 +0100

    Corrige build JS para dist

---
+++ dist/scripts/main.js
@@ -0,0 +1,15 @@
+    document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('form-sorteador').addEventListener('submit', function (event) {
+        event.preventDefault();
+        let numeroMaximo = parseInt(document.getElementById('numero-maximo').value);
+        
+        if (isNaN(numeroMaximo) || numeroMaximo < 2) {
+        alert("Digite um número maior que 1");
+        return;
+        }
+
+        let numeroAleatorio = Math.floor(Math.random() * numeroMaximo) + 1;
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    });
+    });
